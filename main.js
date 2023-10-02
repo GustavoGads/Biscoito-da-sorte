4cf730d1817a468847451b396a6a1e8c8f608cf3
Gustavo Almeida <gustavoalmeida.201632@gmail.com>
2023-10-02 15:08:36 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+/* Variáveis */
+const screen01 = document.querySelector('.screen01')
+const screen02 = document.querySelector('.screen02')
+const imgClick = document.querySelector('#imgClick')
+const btnReset = document.querySelector('#btnReset')
+
+let phrase = document.getElementById('phrase')
+const massageData = [
+    'Se alguém está tão cansado que não possa te dar um sorriso, deixa-lhe o teu.',
+    'Que o dia comece bem e termine ainda melhor.',
+    'Às vezes as coisas demoram, mas acontecem. O importante é saber esperar e não perder a fé!',
+    'Um pequeno pensamento positivo pela manhã pode mudar todo o seu dia.',
+]
+
+/* Eventos */
+imgClick.addEventListener('click', handleClick)
+btnReset.addEventListener('click', handleClickReset)
+
+
+/* Funções */
+
+function toggleScreen() {
+    screen01.classList.toggle('hide')
+    screen02.classList.toggle('hide')
+}
+
+
+function handleClick() {
+    toggleScreen()
+    let randomMassage = Math.floor(Math.random() * massageData.length)
+    
+    phrase.textContent = massageData[randomMassage]
+}
+
+
+function handleClickReset() {
+    toggleScreen() 
+}
